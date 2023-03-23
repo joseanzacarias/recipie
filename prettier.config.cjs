dabf6b82d610230f6273347f33d906f694782579
Oscar C <oacs1221@gmail.com>
2023-03-23 11:44:35 -0400

    feat: add basic schema for recipes
    - added prettier plugin for prisma schema

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("prettier-plugin-prisma"),
+  ],
 };
-
 module.exports = config;
