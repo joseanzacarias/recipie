561d22a4afbacc301c72666f401c3d4a97075722
Oscar C <oacs1221@gmail.com>
2023-03-23 19:24:57 -0400

    feat: basic crud router for recipe

---
+++ src/server/api/routers/recipe.ts
@@ -0,0 +1,58 @@
+import { RecipeModel } from "prisma/zod";
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const recipeRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) =>
+    ctx.prisma.recipe.findMany({
+      where: { userId: ctx.session.user.id },
+    })
+  ),
+
+  getDetailsById: protectedProcedure
+    .input(z.string())
+    .query(({ ctx, input: recipeId }) =>
+      ctx.prisma.recipe.findFirstOrThrow({
+        where: {
+          id: recipeId,
+          userId: ctx.session.user.id,
+        },
+        include: {
+          ingredients: true,
+          steps: true,
+        },
+      })
+    ),
+
+  create: protectedProcedure
+    .input(RecipeModel.omit({ id: true, userId: true }))
+    .mutation(({ ctx, input }) =>
+      ctx.prisma.recipe.create({
+        data: {
+          ...input,
+          userId: ctx.session.user.id,
+        },
+      })
+    ),
+
+  update: protectedProcedure
+    .input(RecipeModel)
+    .mutation(({ ctx, input: recipeInput }) =>
+      ctx.prisma.recipe.updateMany({
+        where: { id: recipeInput.id, userId: ctx.session.user.id },
+        data: recipeInput,
+      })
+    ),
+
+  delete: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input: recipeId }) =>
+      ctx.prisma.recipe.deleteMany({
+        where: {
+          id: recipeId,
+          userId: ctx.session.user.id,
+        },
+      })
+    ),
+});
