da5d284918738604cb04f01d53365814186c3dfe
joseanzacarias <58693390+joseanzacarias@users.noreply.github.com>
2023-03-23 19:55:07 -0400

    feat: :rocket: Ingredients router and crud (#6)
    * feat: :rocket: Ingredients router and crud
    
    * chore: unnecessary awaits removed

---
+++ src/server/api/routers/ingredient.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const ingredientRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) =>
+    ctx.prisma.ingredient.findMany({ where: { userId: ctx.session.user.id } })
+  ),
+
+  getById: protectedProcedure.input(z.string()).query(({ input, ctx }) =>
+    ctx.prisma.ingredient.findFirst({
+      where: { id: input, userId: ctx.session.user.id },
+    })
+  ),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        unit: z.string(),
+        unitPerPrice: z.number().nullable(),
+        price: z.number().nullable(),
+      })
+    )
+    .mutation(({ ctx, input }) =>
+      ctx.prisma.ingredient.create({
+        data: { ...input, userId: ctx.session.user.id },
+      })
+    ),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string().optional(),
+        unit: z.string().optional(),
+        unitPerPrice: z.number().nullable().optional(),
+        price: z.number().nullable().optional(),
+      })
+    )
+    .mutation(({ ctx, input }) =>
+      ctx.prisma.ingredient.updateMany({
+        where: { id: input.id, userId: ctx.session.user.id },
+        data: {
+          ...input,
+        },
+      })
+    ),
+
+  delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) =>
+    ctx.prisma.ingredient.deleteMany({
+      where: { id: input, userId: ctx.session.user.id },
+    })
+  ),
+});
