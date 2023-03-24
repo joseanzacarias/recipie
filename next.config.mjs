0a66f15cd5b10f94667ab14e327072e350dffb17
Oscar C <oacs1221@gmail.com>
2023-03-24 11:02:18 -0400

    wip(front): recipes view

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ],
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
